81a65353490003f08b3977da92787cc52c2b4d36
Gabriel Bursi <gabriellbursii@gmail.com>
2022-12-27 16:17:00 -0300

    editando posts

--- routes/posts.js
+++ routes/posts.js
@@ -23,6 +23,40 @@ router.get('/lista/add', (req, res) => {
         })
 })
 
+router.get('/lista/edit/:id', (req, res) => {
+
+    ModelPostagens.findById(req.params.id).lean().then(postagem => {
+        ModelCategoria.find().lean().then(categorias => {
+            res.render('posts/edit', {postagem, categorias})
+        }).catch(()=> {
+            req.flash('error_msg', 'Houve um erro ao listar categorias')
+            res.redirect('/posts/lista')
+        })
+    }).catch(() => {
+        req.flash('error_msg', 'Houve um erro ao carregar o formulário de edição')
+        res.redirect('/posts/lista')
+    })
+})
+
+router.post('/edit', (req, res) => {
+    ModelPostagens.findById(req.body.id).then(postagem => {
+
+        postagem.titulo = req.body.titulo
+        postagem.slug = req.body.slug
+        postagem.conteudo = req.body.conteudo
+        postagem.categoria = req.body.categoria
+        postagem.descricao = req.body.descricao
+
+        postagem.save().then(() => {
+            req.flash('success_msg', 'Editada com sucesso!')
+            res.redirect('/posts/lista')
+        }).catch(() => {
+            req.flash('error_msg', 'Houve um erro ao editar')
+            res.redirect('/posts/lista')
+        })
+    })
+})
+
 router.post('/nova', (req, res) => {
     const erros = []
 
